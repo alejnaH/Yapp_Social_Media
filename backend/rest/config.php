a73b89fb6613cdd479b93c41ba87d67dbccffdd6
Alejna <alejna.igrice@gmail.com>
2025-12-26 17:47:24 +0100

    db config setup

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -7,27 +7,39 @@ error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
 class Config
 {
-   public static function DB_NAME()
-   {
-       return 'database_yapp'; 
-   }
-   public static function DB_PORT()
-   {
-       return  3306;
-   }
-   public static function DB_USER()
-   {
-       return 'root';
-   }
-   public static function DB_PASSWORD()
-   {
-       return 'root123';
-   }
-   public static function DB_HOST()
-   {
-       return 'localhost';
-   }
-   public static function JWT_SECRET() {
-       return 'your_key_string_key';
-   }
+    public static function DB_NAME()
+    {
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+        return ltrim($dbparts['path'], '/');
+    }
+    public static function DB_PORT()
+    {
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+        return $dbparts['port'];
+    }
+    public static function DB_USER()
+    {
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+        return $dbparts['user'];
+    }
+    public static function DB_PASSWORD()
+    {
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+        return $dbparts['pass'];
+    }
+    public static function DB_HOST()
+    {
+        $url = getenv('JAWSDB_MARIA_URL');
+        $dbparts = parse_url($url);
+        return $dbparts['host'];
+    }
+
+    public static function JWT_SECRET()
+    {
+        return 'your_key_string_key';
+    }
 }
