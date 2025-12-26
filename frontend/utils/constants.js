8fa0358fa1e8ae791c980194bd21eb84b89e08c9
Alejna <alejna.igrice@gmail.com>
2025-12-26 18:05:22 +0100

    deploy url

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,15 +1,6 @@
 let Constants = {
-   PROJECT_BASE_URL: (() => {
-       const hostname = window.location.hostname;
-       const port = window.location.port;
+   PROJECT_BASE_URL: 'https://yapp-backend-b1a90d0d7142.herokuapp.com/',
 
-       if (port && port !== '80' && port !== '443') {
-           return `http://${hostname}:8000/`;
-       }
-
-       return 'http://localhost/Yapp/backend/';
-   })(),
-   
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 }
